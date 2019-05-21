f064da0af2faf32f5fd99736c47dbd7b7f9200e1
Catherine <catherinelu97@gmail.com>
2019-05-21 00:27:25 -0400

    modified css and added project files

---
+++ dev/scripts/fadetoggle.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $('.contact').click(function () {
+        $('.email').fadeToggle('slow');
+    });
+    $('.webdesign').click(function () {
+        $('.webdesign-info').fadeToggle('slow');
+    });
+});
---
+++ dev/sketch/scripts/transpiled/change-img.js
@@ -0,0 +1,5 @@
+$(document).ready(function () {
+    $(`.artworks`).change(function () {
+        $(`img[id=image-swap]`).attr(`src`, $(this).val());
+    });
+});
